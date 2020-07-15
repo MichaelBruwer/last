105d912fbb9a4d00f24e7c498e39d7e2e0573719
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-07-15 16:37:51 +0200

    backend mostly complete-needs tweaks

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 //Connect database
 connectDB();
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the Rateing app' }));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Rateing app Api' }));
 
 //Define Routes
-app.use('/api/users'), require('./routes/users');
-app.use('/api/companys'), require('./routes/companys');
-app.use('/api/auth'), require('./routes/auth');
+app.use('/api/users', require('./routes/users'));
+app.use('/api/companys', require('./routes/companys'));
+app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listhen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
