b4db5c4ed0f6d797a938400862a40ebbd2f6825a
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-07-28 01:08:38 +0200

    prep deploy heroku

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the Rateing app Api' }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
+//Serve statoc assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
